007600b6190004f56ac25bf91e152a75271b9e0a
Chisomaga Anyasodor <chisomanyasodor@gmail.com>
2026-07-21 21:01:43 +0100

    Configure public and private route tables

---
+++ terraform/route_tables.tf
@@ -0,0 +1,54 @@
+# Public Route Table
+
+resource "aws_route_table" "public" {
+
+  vpc_id = aws_vpc.main.id
+
+  route {
+
+    cidr_block = "0.0.0.0/0"
+
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-route-table"
+  }
+}
+
+
+# Associate Public Subnets
+
+resource "aws_route_table_association" "public" {
+
+  count = length(aws_subnet.public)
+
+  subnet_id = aws_subnet.public[count.index].id
+
+  route_table_id = aws_route_table.public.id
+}
+
+
+
+# Private Route Table
+
+resource "aws_route_table" "private" {
+
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-private-route-table"
+  }
+}
+
+
+# Associate Private Subnets
+
+resource "aws_route_table_association" "private" {
+
+  count = length(aws_subnet.private)
+
+  subnet_id = aws_subnet.private[count.index].id
+
+  route_table_id = aws_route_table.private.id
+}
