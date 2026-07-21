f7f693f3312f4e54686caea3776716a851d75968
Chisomaga Anyasodor <chisomanyasodor@gmail.com>
2026-07-21 20:48:29 +0100

    Add public and private subnet architecture

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,29 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+variable "availability_zones" {
+  description = "Availability Zones"
+  type        = list(string)
+  default = [
+    "us-east-1a",
+    "us-east-1b"
+  ]
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDR blocks"
+  type        = list(string)
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private subnet CIDR blocks"
+  type        = list(string)
+  default = [
+    "10.0.3.0/24",
+    "10.0.4.0/24"
+  ]
+}
