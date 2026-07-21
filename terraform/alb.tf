ac5134725f31f1bd0b4bc228e7531a87f8e01961
Chisomaga Anyasodor <chisomanyasodor@gmail.com>
2026-07-21 22:48:33 +0100

    Add ALB HTTP listener

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -57,3 +57,20 @@ resource "aws_lb_target_group" "app" {
     Name = "${var.project_name}-tg"
   }
 }
+
+resource "aws_lb_listener" "http" {
+
+  load_balancer_arn = aws_lb.app.arn
+
+  port = 80
+
+  protocol = "HTTP"
+
+  default_action {
+
+    type = "forward"
+
+    target_group_arn = aws_lb_target_group.app.arn
+
+  }
+}
