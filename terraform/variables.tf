0bf2618982007a267d3fa6d5da607da36a3ee2a1
Chisomaga Anyasodor <chisomanyasodor@gmail.com>
2026-07-21 17:28:00 +0100

    Add AWS VPC infrastructure with Terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Project Name"
+  type        = string
+  default     = "aws-two-tier-app"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
