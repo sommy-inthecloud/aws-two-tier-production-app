5efbea7fb1e97bd905e7d16c2347fd681f57b5f0
Chisomaga Anyasodor <chisomanyasodor@gmail.com>
2026-07-21 21:27:51 +0100

    Add IAM role and instance profile for EC2

---
+++ terraform/iam.tf
@@ -0,0 +1,42 @@
+# IAM Role for EC2
+
+resource "aws_iam_role" "ec2_role" {
+  name = "${var.project_name}-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project_name}-ec2-role"
+  }
+}
+
+
+# Attach AmazonSSMManagedInstanceCore Policy
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+
+# Create Instance Profile
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "${var.project_name}-instance-profile"
+
+  role = aws_iam_role.ec2_role.name
+}
